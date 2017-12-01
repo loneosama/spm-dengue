25186c378678766dfb09291df0d81e2e72525297
loneosama <osama.arif@khi.iba.edu.pk>
2017-12-01 17:18:37 -0500

    done debugging

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -89,7 +89,7 @@ class RoleController extends Controller
         $role = Role::find($id);
         $permission = Permission::get();
         $rolePermissions = DB::table("permission_role")->where("permission_role.role_id",$id)
-            ->lists('permission_role.permission_id','permission_role.permission_id');
+            ->pluck('permission_role.permission_id','permission_role.permission_id')->all();
 
         return view('roles.edit',compact('role','permission','rolePermissions'));
     }
